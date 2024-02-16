95374897dc6922fa5cc06b95b288fa5fdb06b562
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-02-16 20:32:17 +0530

    mm

--- Ochi/src/App.jsx
+++ Ochi/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className='w-full h-screen bg-zinc-900'>
+    <div className='w-full h-screen bg-zinc-900 text-white'>
       <Navbar/>
     </div>
   )
