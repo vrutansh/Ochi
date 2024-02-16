b3f7e9887943bf6d70bcff7d006572c2592520c1
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-02-16 22:24:22 +0530

    nm

--- Ochi/src/App.jsx
+++ Ochi/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import Landing from './components/Landing';
+
 function App() {
   return (
-    <div className='w-full h-screen bg-zinc-900 text-white'>
+    <div className='text-white'>
       <Navbar/>
+      <Landing/>
     </div>
   )
 }
---
+++ Ochi/src/components/Landing.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div className="w-full h-screen bg-zinc-900 pt-2 ">
+        <div className="textstructue mt-32  px-20">
+            <div className="masker">
+                <h1 className="uppercase text-9xl leading-none tracking-tighter font-lg font-md font-['Futura']">We Create</h1>
+            </div>
+
+            <div className="masker">
+               <h1 className="uppercase text-9xl leading-none tracking-tighter font-lg font-md font- [' ']">We Create</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Landing
