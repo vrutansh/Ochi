ba4266236add4a0428b781f48870f4a82c7ea9cb
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-02-17 15:46:18 +0530

    landing

--- Ochi/src/components/Landing.jsx
+++ Ochi/src/components/Landing.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-
+import { FaArrowUpLong } from "react-icons/fa6";
 function Landing() {
   return (
-    <div className="w-full h-screen bg-zinc-900 pt-2 ">
-        <div className="textstructue mt-32  px-20">
-            <div className="masker">
-                <h1 className="uppercase text-9xl leading-none tracking-tighter font-lg font-md font-['Futura']">We Create</h1>
-            </div>
+    <div className="w-full  bg-zinc-900 pt-2 ">
+        <div className="textstructue mt-52  px-20">
+          {["We Create", "Eye Opening","Presentations"].map((item,index)=>(
+              <div className="masker">
+               <div className='w-fit flex items-center'>
+               <h1 key={index} className="uppercase text-[9.5vw] leading-[7.5vw] tracking-normal font-lg font-bold font-['Test_Founders_Grotesk_X-Cond_SmBd']">{item}</h1>
+               </div>
+           </div>
+          ))}
+        </div>
+        <div className='border-t-[1px] border-zinc-700 mt-32 flex justify-between items-center py-5 px-20'>
 
-            <div className="masker">
-               <h1 className="uppercase text-9xl leading-none tracking-tighter font-lg font-md font- [' ']">We Create</h1>
+          {["For public and private companies","From the Pitch to IPO"].map((items,index)=>(
+             <p key={index} className='text-md font-light tracking-tight leading-none'>{items}</p>
+          ))}
+            <div className='start flex items-center gap-5'>
+                  <div className='px-4 py-2 border-[1px] font-light text-md uppercase capitalize border-zinc-500 rounded-full'>Start the Project</div>
+
+                  <div className='w-10 h-10 flex items-center justify-center border-[1px] border-zinc-500 rounded-full'><span className='rotate-[45deg]'><FaArrowUpLong/></span></div>
             </div>
         </div>
     </div>
